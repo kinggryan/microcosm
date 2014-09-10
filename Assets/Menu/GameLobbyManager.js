a8073d5c70109392b6d1c26fc6278437fa8357ca
Ryan King <kinggryan@gmail.com>
2014-09-09 18:55:17 -0700

    Imported Photon

---
+++ Assets/Menu/GameLobbyManager.js
@@ -0,0 +1,54 @@
+﻿#pragma strict
+
+/*****
+
+	Game Lobby class
+	
+	Duties include:
+	-Joining random lobby or starting one if needed on start
+	-On a button press, send an rpc to others in this lobby to go to the game room.
+	
+	*****/
+	
+class GameLobbyManager extends Photon.MonoBehaviour {
+	// Properties
+	
+	// Methods
+	    // Use this for initialization
+    function Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+        
+    }
+
+    function OnJoinedLobby()
+    {
+        Debug.Log("JoinRandom");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    function OnPhotonRandomJoinFailed()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    function OnJoinedRoom()
+    {
+        Debug.Log("Room Joined");
+        photonView.viewID = 5;
+    }
+
+    function OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString() + ". Player count: "+PhotonNetwork.countOfPlayers+" in room " +PhotonNetwork.room);
+    
+    	if(GUI.Button(Rect(15,15,100,100),"Start Game"))
+    		photonView.RPC("StartGame",PhotonTargets.All);
+    }
+    
+    @RPC
+    function StartGame() {
+    	// Load Game level, 1
+    	PhotonNetwork.LoadLevel(1);	
+    }
+}
